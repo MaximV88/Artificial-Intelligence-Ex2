73330a576d5c75c06a2801b22b4fec344f2ad153
Maxim Vainshtein <Maxim@maxims-macbook-pro.local>
2015-12-15 20:11:56 +0200

    Initial Commit

---
+++ Ex2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ex2
+//
+//  Created by Maxim Vainshtein on 15/12/2015.
+//  Copyright © 2015 Maxim Vainshtein. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
